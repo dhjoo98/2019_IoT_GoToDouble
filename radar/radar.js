09c58871482accfdc0ea3c87e03ee56ce09583a0
corikachu <konomama0505@gmail.com>
2019-10-23 01:04:57 +0900

    add ajax call to sever for pulling data contained angle and distance

--- radar/radar.js
+++ radar/radar.js
@@ -2,18 +2,17 @@
 
 var nowAngle, nowDistance;
 var cFont;
-var data_url = "";
+var radar1_url = "http://127.0.0.1:5000/fposition";
+var radar2_url = "http://127.0.0.1:5000/sposition";
+
 
 function setup() {
-  var cnv = createCanvas(windowWidth, windowHeight);
-
+  var cnv = createCanvas(windowWidth*0.95+31, windowHeight*1.01-50);
   background("#111");
   smooth();
 
-  // TODO: fix font issue
-  //cFont = loadFont('Roboto');
-  nowAngle = 10;
-  nowDistance = 10;
+  //nowAngle = 10;
+  //nowDistance = 15;
 }
 
 function windowResized() {
@@ -24,14 +23,31 @@ function windowResized() {
 
 function draw() {
   fill(98,245,31);
+  noStroke();
+  fill(0,4);
+  rect(0, 0, width, height-height*0.065);
+  // get data from server
+  getDataFromServer();
 
-  // font issue
-  //textFont(cFont);
-
+  fill(98,245,31);
   drawRadarDefaultShape();
   drawSweepLine();
   drawInfoText();
   drawObject();
+}
+
+function getDataFromServer() {
+
+  $.ajax({ type: "GET", url: radar1_url, dataType: 'json', success: function(response){
+      //var json_obj = $.parseJSON(response);
+      if (response.code == -1) {
+        return;
+      }
+      nowAngle = float(response.angle)
+      nowDistance = float(response.distance)
+      // console.log(json_obj.time)
+  } });
+
 }
 
 /* Drawing Radar Shape */
@@ -51,7 +67,8 @@ function drawRadarDefaultShape() {
   arc(0,0,(width-width*0.687),(width-width*0.687),PI,TWO_PI);
 
   // drawing line
-  line(-width/2,0,width/2,0);
+  line(-width/2, 0, width/2, 0);
+  console.log(width/2)
   line(0,0,(-width/2)*cos(radians(30)),(-width/2)*sin(radians(30)));
   line(0,0,(-width/2)*cos(radians(90)),(-width/2)*sin(radians(90)));
   line(0,0,(-width/2)*cos(radians(60)),(-width/2)*sin(radians(60)));
@@ -74,7 +91,7 @@ function drawSweepLine() {
 
 function drawInfoText() {
   push();
-  noFill();
+  fill(0,0,0);
   noStroke();
   rect(0, height-height*0.0648, width, height);
   fill(98, 245, 31);
@@ -88,7 +105,7 @@ function drawInfoText() {
 
   // write Infomation like degree
   textSize(30);
-  text("Angle: " + nowAngle +" °", width-width*0.48, height-height*0.0277);
+  text("Angle: " + nowAngle +" ", width-width*0.48, height-height*0.0277);
   text("Distance: ", width-width*0.26, height-height*0.0277);
   if(nowDistance<40) {
   text("        " + nowDistance +" cm", width-width*0.225, height-height*0.0277);
@@ -99,27 +116,27 @@ function drawInfoText() {
   fill(98, 245, 60);
   translate((width-width*0.495)+width/2*cos(radians(30)), (height-height*0.0907)- width/2*sin(radians(30)));
   rotate(-radians(-60));
-  text("30°",0,0)
+  text("30",0,0)
   resetMatrix();
 
   translate((width-width*0.503)+width/2*cos(radians(60)),(height-height*0.0888)-width/2*sin(radians(60)));
   rotate(-radians(-30));
-  text("60°",0,0);
+  text("60",0,0);
   resetMatrix();
 
   translate((width-width*0.507)+width/2*cos(radians(90)),(height-height*0.0833)-width/2*sin(radians(90)));
   rotate(radians(0));
-  text("90°",0,0);
+  text("90",0,0);
   resetMatrix();
 
   translate(width-width*0.513+width/2*cos(radians(120)),(height-height*0.07129)-width/2*sin(radians(120)));
   rotate(radians(-30));
-  text("120°",0,0);
+  text("120",0,0);
   resetMatrix();
 
   translate((width-width*0.5104)+width/2*cos(radians(150)),(height-height*0.0574)-width/2*sin(radians(150)));
   rotate(radians(-60));
-  text("150°",0,0);
+  text("150",0,0);
 
   pop();
 }
