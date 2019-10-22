44a65c5ae6d7e577bc36bf702a259d92193010e5
dhjoo98 <dhjoo98@hanmail.net>
2019-10-22 18:00:25 +0000

    raspi-part added, not working
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect.py
+++ arduino_raspi_connection/raspi/connect.py
@@ -1,14 +1,12 @@
 from bluetooth import *
-import requests,json
 
-url = "http://127.0.0.1:5000/fposition"
-client_socket=BluetoothSocket(RFCOMM)
+client_socket=BluetootSocket(RFCOMM)
 
 client_socket.connect(("98:D3:71:FD:7C:19",1)) #MAC of first HC06
 
 while True:
-    distance,angle = map(float,client_socket.recv(1024).split())
-    data = {"angle": str(angle), "distance": str(distance)}
-    requests.post(url=url,data=data)
-
+    msg = client_socket.recv(1024)
+    print("received message :{}".format(msg))
+    
+print("Finished")
 client_socket.close()
