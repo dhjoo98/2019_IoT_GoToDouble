3fbc51dc651a01ad0b81d8ad2a90fcd00694a1c7
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 12:42:56 +0000

    connect_2 also added
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect_2.py
+++ arduino_raspi_connection/raspi/connect_2.py
@@ -10,11 +10,19 @@ client_socket.connect(("98:D3:51:FD:AB:20",1))
 i = 0;
 
 while True:
-    msg = client_socket.recv(1024)
-    try :
-        int(msg)
-        print("received message :{}".format(msg))
-    except:
+    msg = client_socket.recv(255) #1024
+    
+    if msg == '[':
         continue
+    else :    
+        msg = msg.replace('[','') #all sorts of exceptions
+        msg = msg.replace('\r','')
+        dic = msg.split('\n')
+        try:
+            print([int(dic[0]),int(dic[1])]) #also exception-proof
+        except:
+            #print('error')
+            continue
+    
 print("Finished")
 client_socket.close()
