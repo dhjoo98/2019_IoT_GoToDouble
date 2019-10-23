4602401d9191345666e81e033a67df4e0956c685
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 08:50:42 +0000

    connect.py back to original
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect_ble_ver.py
+++ arduino_raspi_connection/raspi/connect_ble_ver.py
@@ -4,10 +4,8 @@ client_socket.connect(("98:D3:71:FD:7C:19",1))
 
 while True:
     msg = client_socket.recv(1024)
-    try :
-        int(msg)
-        print("received message :{}".format(msg))
-    except:
-        continue
+    print("received message :{}".format(msg))
+  
 print("Finished")
 client_socket.close()
+>>>>>>> connect.py back to original
