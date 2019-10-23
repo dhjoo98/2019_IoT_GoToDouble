3bcc56d9474467e0a50adf09e2f628cf1d18711d
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 06:42:41 +0000

    Fix to both connect.py s
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect_2.py
+++ arduino_raspi_connection/raspi/connect_2.py
@@ -10,19 +10,11 @@ client_socket.connect(("98:D3:51:FD:AB:20",1))
 i = 0;
 
 while True:
-    msg = client_socket.recv(255) #1024
-    
-    if msg == '[':
+    msg = client_socket.recv(1024)
+    try :
+        int(msg)
+        print("received message :{}".format(msg))
+    except:
         continue
-    else :    
-        msg = msg.replace('[','') #all sorts of exceptions
-        msg = msg.replace('\r','')
-        dic = msg.split('\n')
-        try:
-            print([int(dic[0]),int(dic[1])]) #also exception-proof
-        except:
-            #print('error')
-            continue
-    
 print("Finished")
 client_socket.close()
