780ebc1373de7d5fae7da5373a8339090b2a7beb
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 06:42:41 +0000

    Fix to both connect.py s
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect_2.py
+++ arduino_raspi_connection/raspi/connect_2.py
@@ -7,8 +7,19 @@ client_socket=BluetoothSocket(RFCOMM)
 client_socket.connect(("98:D3:51:FD:AB:20",1)) #address of 2nd HC06
 
 while True:
+<<<<<<< HEAD
     distance,angle = map(float,client_socket.recv(1024).split())
     data = {"angle": str(angle), "distance": str(distance)}
     requests.post(url=url,data=data)
 
 client_socket.close()
+=======
+    msg = client_socket.recv(1024)
+    try :
+        int(msg)
+        print("received message :{}".format(msg))
+    except:
+        continue
+print("Finished")
+client_socket.close()
+>>>>>>> Fix to both connect.py s
