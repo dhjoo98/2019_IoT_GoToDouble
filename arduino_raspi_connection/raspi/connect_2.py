1a27597fc093dd4576e7f8b5c26dfdaae9b2a9cd
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 12:42:56 +0000

    connect_2 also added
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect_2.py
+++ arduino_raspi_connection/raspi/connect_2.py
@@ -1,25 +1,28 @@
+#Must be run after arduino starts sending
 from bluetooth import *
 import requests,json
 
 url = "http://127.0.0.1:5000/sposition"
 client_socket=BluetoothSocket(RFCOMM)
 
-client_socket.connect(("98:D3:51:FD:AB:20",1)) #address of 2nd HC06
+client_socket.connect(("98:D3:51:FD:AB:20",1))
+
+i = 0;
 
 while True:
-<<<<<<< HEAD
-    distance,angle = map(float,client_socket.recv(1024).split())
-    data = {"angle": str(angle), "distance": str(distance)}
-    requests.post(url=url,data=data)
-
-client_socket.close()
-=======
-    msg = client_socket.recv(1024)
-    try :
-        int(msg)
-        print("received message :{}".format(msg))
-    except:
+    msg = client_socket.recv(255) #1024
+    
+    if msg == '[':
         continue
+    else :    
+        msg = msg.replace('[','') #all sorts of exceptions
+        msg = msg.replace('\r','')
+        dic = msg.split('\n')
+        try:
+            print([int(dic[0]),int(dic[1])]) #also exception-proof
+        except:
+            #print('error')
+            continue
+    
 print("Finished")
 client_socket.close()
->>>>>>> Fix to both connect.py s
