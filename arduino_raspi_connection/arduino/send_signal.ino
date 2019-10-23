35126ac89292c835109d3781bda9ab958f371ead
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 11:56:10 +0000

    send_signal.ino to fit rader output format
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/arduino/send_signal.ino
+++ arduino_raspi_connection/arduino/send_signal.ino
@@ -1,20 +1,27 @@
-//각 아두이노에 업로드 
 #include <SoftwareSerial.h>
  
 #define BT_RXD 8 //To HC06 TXD
 #define BT_TXD 7 //To HC06 RXD
-SoftwareSerial bluetooth(BT_RXD, BT_TXD); //해당 장치로 시리얼 아웃풋 출력. 
+SoftwareSerial bluetooth(BT_RXD, BT_TXD);
  
 void setup(){
-  int test_number; 
+  int i;
+  int tag;
+  //int test_number_1;
+  //int test_number_2;
   Serial.begin(9600);
   bluetooth.begin(9600);
 }
  
 void loop(){
     //Read Sensor here.
-    int test_number = random(9);
-    Serial.println(test_number);
-    bluetooth.println(test_number);
+    for (int i = 10; i< 2000; i++){
+      int tag = 2000-i;
+      bluetooth.write("[");
+      Serial.println(i);
+      bluetooth.println(i);
+      bluetooth.println(tag);
+      delay(1000);
+    }
     delay(2000);
 }
