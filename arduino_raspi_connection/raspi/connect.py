a4c8111b2a01c0f2a6395f9d8477eb19a737761d
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 18:14:18 +0000

    receiving data format to fit Django
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect.py
+++ arduino_raspi_connection/raspi/connect.py
@@ -1,25 +1,30 @@
 #Must be run after arduino starts sending
+#delay-proof
 from bluetooth import *
 
 client_socket=BluetoothSocket(RFCOMM)
 
-client_socket.connect(("98:D3:71:FD:7C:19",1))
+client_socket.connect(("98:D3:91:FD:83:4B",1))
 
 i = 0;
-
+'''
+msg = 'AT'
+client_socket.send(msg)
+print(msg)
+'''
 while True:
     msg = client_socket.recv(255) #1024
+    #print(msg)
     
     if msg == '[':
         continue
-    else :    
+    else :  
         msg = msg.replace('[','') #all sorts of exceptions
         msg = msg.replace('\r','')
         dic = msg.split('\n')
         try:
-            print([int(dic[0]),int(dic[1])]) #also exception-proof
+            print(float(dic[0]),float(dic[1])) #also exception-proof
         except:
-            #print('error')
             continue
     
 print("Finished")
