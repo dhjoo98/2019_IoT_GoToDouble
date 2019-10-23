0944fca91a2ae3296db59ed0d9733809528d3c5a
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 06:29:57 +0000

    Problem with blank message received fixed
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect.py
+++ arduino_raspi_connection/raspi/connect.py
@@ -1,22 +1,13 @@
-from bluepy import btle
-import struct
+client_socket=BluetoothSocket(RFCOMM)
 
-class MyDelegate(btle.DefaultDelegate):
-    def __init__(self,params):
-        btle.DefaultDelegate.__init__(self)
-
-    def handleNotification(self,cHandle,data):
-        print("handling notification...")
-##        print(self)
-##        print(cHandle)
-        print(struct.unpack("b",data))
-
-p = btle.Peripheral('00:15:87:00:4e:d4')
-p.setDelegate(MyDelegate(0))
+client_socket.connect(("98:D3:71:FD:7C:19",1))
 
 while True:
-    if p.waitForNotifications(1.0):
+    msg = client_socket.recv(1024)
+    try :
+        int(msg)
+        print("received message :{}".format(msg))
+    except:
         continue
-    print("waiting...")
-
-#source https://makersportal.com/blog/2018/3/25/arduino-internet-of-things-part-4-connecting-bluetooth-nodes-to-the-raspberry-pi-using-pythons-bluepy-library
+print("Finished")
+client_socket.close()
