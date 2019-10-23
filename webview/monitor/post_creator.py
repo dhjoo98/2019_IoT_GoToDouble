2fe2585469c7d7c8d76fd010fce89c19f256aa40
epikjjh <epikjjh@gmail.com>
2019-10-23 15:58:38 +0900

    Modify views.py modify test code
    Signed-off-by: epikjjh <epikjjh@gmail.com>

--- webview/monitor/post_creator.py
+++ webview/monitor/post_creator.py
@@ -1,8 +1,12 @@
 import requests,json
-furl = "http://127.0.0.1:8000/fposition"
-surl = "http://127.0.0.1:8000/sposition"
+furl = "http://127.0.0.1:5000/fposition"
+surl = "http://127.0.0.1:5000/sposition"
+angle = 0.0
+distance = 0.0
 while True:
-    dummy_fdata = {"angle": "90.0", "distance":"125.0"}
-    dummy_sdata = {"angle": "80.0", "distance":"124.0"}
+    dummy_fdata = {"angle": str(angle), "distance":str(distance)}
+    dummy_sdata = {"angle": str(angle+1.2), "distance":str(distance+0.2)}
     fpost = requests.post(url=furl,data=dummy_fdata)
     spost = requests.post(url=surl,data=dummy_sdata)
+    angle = 0 if angle>180 else angle+1
+    distance = 0 if distance>160 else distance+1
