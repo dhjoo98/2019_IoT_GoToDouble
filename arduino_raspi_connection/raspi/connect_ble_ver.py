fb4e95e1eb2818ac7bb05b5be3dca8e4cccc698e
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 06:46:25 +0000

    fixing merge error
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect_ble_ver.py
+++ arduino_raspi_connection/raspi/connect_ble_ver.py
@@ -1,26 +1,13 @@
-#Must be run after arduino starts sending
-from bluetooth import *
-
-client_socket=BluetootSocket(RFCOMM)
+client_socket=BluetoothSocket(RFCOMM)
 
 client_socket.connect(("98:D3:71:FD:7C:19",1))
 
-i = 0;
-
 while True:
-    msg = client_socket.recv(255) #1024
-    
-    if msg == '[':
+    msg = client_socket.recv(1024)
+    try :
+        int(msg)
+        print("received message :{}".format(msg))
+    except:
         continue
-    else :    
-        msg = msg.replace('[','') #all sorts of exceptions
-        msg = msg.replace('\r','')
-        dic = msg.split('\n')
-        try:
-            print([int(dic[0]),int(dic[1])]) #also exception-proof
-        except:
-            #print('error')
-            continue
-    
 print("Finished")
 client_socket.close()
