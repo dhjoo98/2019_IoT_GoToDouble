d9622543e78e17a1de1d22ae36fed11c9d3b38f0
dhjoo98 <dhjoo98@hanmail.net>
2019-10-23 06:42:41 +0000

    Fix to both connect.py s
    Signed-off-by: dhjoo98 <dhjoo98@hanmail.net>

--- arduino_raspi_connection/raspi/connect_2.py
+++ arduino_raspi_connection/raspi/connect_2.py
@@ -6,7 +6,10 @@ client_socket.connect(("98:D3:51:FD:AB:20",1)) #address of 2nd HC06
 
 while True:
     msg = client_socket.recv(1024)
-    print("received message :{}".format(msg))
-    
+    try :
+        int(msg)
+        print("received message :{}".format(msg))
+    except:
+        continue
 print("Finished")
 client_socket.close()
